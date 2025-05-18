48af519fe09dad96dd2a609267d79bd6eb2a4f6b
gahmarin <gabriel.c.pb@compasso.com.br>
2025-05-18 18:35:33 -0300

    Commit Atividade Componentes na Íntegra

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,5 +1,8 @@
-
+import { getImageUrl } from "./utils";
+import { people } from "./data";
 import React from 'react';
+import './Gallery.css';
+
 
 export function Article() {
     return (
@@ -14,7 +17,7 @@ export function Article() {
     );
 }
 
-export function Profile() {
+export function Profile1() {
     return (
         <img
             src="https://i.imgur.com/QIrZWGIs.jpg"
@@ -23,7 +26,7 @@ export function Profile() {
     );
 }
 
-export function Profile1() {
+export function Profile2() {
     return (
         <img
             src="https://i.imgur.com/MK3eW3As.jpg"
@@ -50,16 +53,36 @@ export function HedyTodos() {
     );
 }
 
-export function Avatar() {
-    const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
-    const description = "Gregorio Y. Zara";
-    return (
+function Avatar({person, size}) {
+    return( 
         <img
             className="avatar"
-            src={avatar}
-            alt={description}
+            src={getImageUrl(person)}
+            alt={person.name}
+            width={size}
+            height={size}
         />
     );
+}
+
+export function Profile(){
+    return(
+        <>
+            <h3>Profile</h3>
+            <Avatar
+                size={100}
+                person={{name: 'Katsuko Saruhashi', imageId: 'YfeOqp2'}}
+            />
+            <Avatar
+                size={100}
+                person={{name: 'Aklilu Lemma', imageId: 'OKS67lh'}}
+            />
+            <Avatar
+                size={100}
+                person={{name: 'Lin Lanying', imageId: '1bX5QH6'}}
+            />
+        </>
+    )
 }
 
 export function TodoList() {
@@ -86,17 +109,109 @@ export function TodoListD() {
     )
 }
 
+export function List() {
+    const peoples = people.filter(person => person.profession);
+    const listItems = peoples.map(person => {
+        return <li key={person.id}>
+            <img
+                src={getImageUrl(person)}
+                alt={person.name}
+            />
+            <p>
+                <b>{person.name}:</b>
+                {' '+person.profession+' '}
+                known for {person.accomplishment}
+            </p>
+        </li>
+    });
+    return <ol>{listItems}</ol>
+}
+
+function Item({ name, isPacked}) {
+    let itemContent = name;
+    if(isPacked) {
+        itemContent = (<del>{name+'✔'}</del>)
+    }
+    return (
+        <li className="item">
+            {itemContent}
+        </li>
+    )
+}
+
+export function PackingList(){
+    return(
+        <section>
+            <h2>Sally Rede's Packing List</h2>
+            <ul>
+                <Item isPacked={true} name="Space Suit" />
+                <Item isPacked={true} name="Helmet with a golden leaf" />
+                <Item isPacked={false} name="Photo of Tam" />
+            </ul>
+        </section>
+    )
+}
+
+function Recipe({ drinkers}) {
+    return (
+        <ol>
+            <li>Boil {drinkers} cups of water.</li>
+            <li>Add {drinkers} spoons of tea and {0.5 * drinkers} spoons</li>
+            <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste</li>
+        </ol>
+    );
+}
+
+export function PrepareMode() {
+    return(
+        <section>
+            <h2>Spiced Chai Recipe</h2>
+            <h3>For two</h3>
+            <Recipe drinkers={2} />
+            <h3>For a gathering</h3>
+            <Recipe drinkers={4} />
+        </section>
+    );
+}
+
+function Cup({ guest }) {
+    return <p>Tea cup for guest #{guest}</p>
+}
+
+// export default function TeaSet(){
+//     return(
+//         <>
+//             <h2>Tea Set</h2>
+//             <Cup guest={1}/>
+//             <Cup guest={2}/>
+//             <Cup guest={3}/>
+//         </>
+//     )
+// }
+export function TeaSet(){
+    let cups = [];
+    for (let i = 1; i <= 6; i++) {
+        cups.push(<Cup key={i} guest={i} />);
+    }
+    return cups;
+}
+
 export default function Gallery() {
     return (
-        <section>
+        <section className= "Gallery">
             <h1>Amazing scientists</h1>
             <Article />
-            <Profile />
             <Profile1 />
+            <Profile2 />
+            <Profile />
             <HedyTodos />
-            <Avatar />
+            <Avatar person ={{name: "Percy Lavon Julian", imageId: "IOjWm71"}} size={80} />
+            <PackingList />
             <TodoList />
             <TodoListD />
+            <List /> 
+            <PrepareMode />
+            <TeaSet />
         </section>
     );
 }
